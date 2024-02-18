dc95b14abcb018ceda35dc3c9970b17bcd691b3f
hsjo12 <hsjo12@naver.com>
2024-02-19 06:38:41 +0900

    update

--- Website/src/components/layout/header.js
+++ Website/src/components/layout/header.js
@@ -53,20 +53,20 @@ export default function Header() {
     );
   } else {
     return (
-      <div className=" flex flex-col    mx-auto w-screen left-0 top-0 fixed items-center justify-end bg-[#00000096]">
+      <div className=" flex flex-col  infoText  mx-auto w-screen left-0 top-0 fixed items-center justify-end bg-[#00000096]">
         <div className=" mx-auto w-[90%] grid grid-cols-2 border-main-color border-b-2 pl-1 pr-1 tracking-[0.15rem]">
-          <button className=" flex flex-col justify-center  items-start w-full h-full tracking-[0.3rem]">
+          <button className=" flex flex-col justify-center  items-start  tracking-[0.3rem]">
             <Link href="/">PLAYWIN</Link>
           </button>
 
           <button
             onClick={() => setOpen(true)}
-            className="flex flex-col justify-center  items-end w-full h-full"
+            className="textBtn flex flex-col justify-center  items-end "
           >
             <FiAlignJustify size="2.5rem" className="zoomInAndOut" />
           </button>
           {open ? (
-            <div className="menuBo flex flex-col infoText items-center justify-center">
+            <div className="menuBox w-full h-full fixed top-0 left-0 flex flex-col infoText items-center justify-center">
               <div className="p-5 ">
                 <Wallet
                   className1="btnText connectButton flex flex-col justify-end items-center p-2"
