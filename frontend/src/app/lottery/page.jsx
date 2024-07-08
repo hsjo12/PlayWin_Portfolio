58ebf7365752a7a9ce108978bc235891d0d65904
hsjo12 <hsjo12@naver.com>
2024-07-09 04:45:05 +0900

    update

--- frontend/src/app/lottery/page.jsx
+++ frontend/src/app/lottery/page.jsx
@@ -2,7 +2,7 @@ import FirstSection from "./firstSection/firstSection";
 import SecondSection from "./secondSection";
 import ThirdSection from "./thirdSection/thirdSection";
 import FourthSection from "./fourthSection/fourthSection";
-import FifthSection from "./FifthSection";
+import FifthSection from "./fifthSection";
 export default async function Lottery() {
   return (
     <main className="largeText font-bebas_neue flex flex-col justify-start items-center mx-auto w-full min-h-[80vh] gap-y-10 xl:gap-y-20">
